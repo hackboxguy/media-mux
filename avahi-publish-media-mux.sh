7595865da22b789e09f6866543a53a04fe011b3d
hackboxguy <albert.david@gmail.com>
2023-03-20 16:56:23 +0100

    fixed ip addr extraction on eth0 interface

--- avahi-publish-media-mux.sh
+++ avahi-publish-media-mux.sh
@@ -2,5 +2,5 @@
 MY_ID_STRING="media-mux-0001"
 MY_ID_PORT=80
 MY_ID_SERVICE="_http._tcp"
-MY_ID_HW=$(ifconfig | grep eth0 | awk '{print $5}')
+MY_ID_HW=$(ifconfig |grep -A1 eth0 |grep inet | awk '{print $2}')
 avahi-publish-service -s "$MY_ID_STRING [$MY_ID_HW]" $MY_ID_SERVICE $MY_ID_PORT
