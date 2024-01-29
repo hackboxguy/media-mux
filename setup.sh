913416409ada045a1afde1f32ab1a0a27b81148e
hackboxguy <albert.david@gmail.com>
2024-01-29 12:30:22 +0100

    copy kodi configs with absolute path

--- setup.sh
+++ setup.sh
@@ -89,9 +89,9 @@ fi
 #useful for raspi-4 with touchscreen based display
 printf "Preparing Kodi-config and desktop shortcuts............. "
 runuser -l pi -c 'mkdir -p /home/pi/.kodi/userdata'
-runuser -l pi -c 'cp sources.xml /home/pi/.kodi/userdata/'
-runuser -l pi -c 'cp guisettings.xml /home/pi/.kodi/userdata/'
-runuser -l pi -c 'cp wf-panel-pi.ini /home/pi/.config/'
+runuser -l pi -c 'cp /home/pi/media-mux/sources.xml /home/pi/.kodi/userdata/'
+runuser -l pi -c 'cp /home/pi/media-mux/guisettings.xml /home/pi/.kodi/userdata/'
+runuser -l pi -c 'cp /home/pi/media-mux/wf-panel-pi.ini /home/pi/.config/'
 cp mediamuxstart.png /usr/share/pixmaps/
 cp mediamuxstop.png /usr/share/pixmaps/
 cp mediamuxstart.png /usr/share/icons/hicolor/48x48/apps/
